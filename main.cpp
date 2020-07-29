b903b5fcb67cebf2f668015fcd58e64651892a56
Sávio <ssoaressousas@gmail.com>
2020-07-29 20:39:00 -0300

    Criação do Projeto

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string nome;
+	cout << "ola visitante, qual seu nome?" << endl;
+	cin >> nome;
+		cout << endl << "Ola " << nome << ", eu nao lembro quase nada de programacao kk";	
+	
+	return 0;
+}
